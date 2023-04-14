4be20b9c2e73333986fd4310c0120d8685418699
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-04-14 15:40:12 +0200

    add "fizz" and "buzz"

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,14 @@
 const eleGrid = document.querySelector(".grid");
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
   console.log(i);
+
+  if (i % 3 == 0) {
+    eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">Fizz</div>`;
+  } else if (i % 5 == 0) {
+    eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">Buzz</div>`;
+  } else if (i % 3 && i % 5 == 0) {
+    eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">FizzBuzz</div>`;
+  } else i !== 0;
   eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
 }
