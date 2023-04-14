a58b53b518df4d915868fb253a73feee71337968
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-04-14 16:17:31 +0200

    fix js and add "FizzBuzz"

--- js/myscript.js
+++ js/myscript.js
@@ -2,13 +2,13 @@ const eleGrid = document.querySelector(".grid");
 
 for (let i = 1; i <= 100; i++) {
   console.log(i);
-
-  if (i % 3 == 0) {
-    eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">Fizz</div>`;
-  } else if (i % 5 == 0) {
-    eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">Buzz</div>`;
-  } else if (i % 3 && i % 5 == 0) {
-    eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">FizzBuzz</div>`;
-  } else i !== 0;
-  eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
+  if (i % 3 === 0 && i % 5 == 0) {
+    eleGrid.innerHTML += `<div class="cell fizzbuzz">FizzBuzz</div>`;
+  } else if (i % 3 === 0) {
+    eleGrid.innerHTML += `<div class="cell fizz">Fizz</div>`;
+  } else if (i % 5 === 0) {
+    eleGrid.innerHTML += `<div class="cell buzz">Buzz</div>`;
+  } else {
+    eleGrid.innerHTML += `<div class="cell">${i}</div>`;
+  }
 }
